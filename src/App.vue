fa08f67b863ffc264d506ea8fc7023552ccda930
Mary8851 <katsha.94@mail.ru>
2023-05-28 23:18:19 +0300

    add vue js project

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <HelloWorld />
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: "App",
   components: {
     HelloWorld,
   },
@@ -15,12 +13,16 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://fonts.googleapis.com/css2?family=Questrial&family=Roboto:wght@300&display=swap");
+* {
+  padding: 0;
+  margin: 0;
+  font-family: "Gilroy";
+}
+a {
+  text-decoration: none;
+}
+div {
+  box-sizing: border-box;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,114 +1,126 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <h1>Список покупок</h1>
+  <div class="wrapper">
+    <div class="list">
+      <div class="list-item" v-for="(list, index) of lists" :key="index">
+        <li @click="deleteTodo(index)">
+          {{ list }}
+        </li>
+      </div>
+      <p v-show="lists.length === 0">
+        Список пустой. Добавьте, пожалуйста, товар
+      </p>
+    </div>
+    <div class="add-item">
+      <input type="text" v-model="input" />
+    </div>
+    <div class="add-btn">
+      <button @click="addItem">Добавить в список покупок</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "HelloWorld",
-  props: {
-    msg: String,
+  data() {
+    return {
+      lists: ["Iphone", "Macbook", "Ipad", "Apple Watch", "iMac"],
+      input: "",
+    };
+  },
+  methods: {
+    addItem() {
+      this.lists.push(this.input);
+      this.input = "";
+    },
+    deleteTodo(index) {
+      this.lists.splice(index, 1);
+    },
   },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
+<style lang="scss">
+* {
+  margin: 0;
   padding: 0;
+  box-sizing: border-box;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+body {
+  background: #4d244e;
+  padding: 30px;
+  height: 100vh;
 }
-a {
-  color: #42b983;
+h1 {
+  font-size: 50px;
+  color: #fff;
+  text-align: center;
+  margin-bottom: 15px;
+}
+.wrapper {
+  margin: auto;
+  padding: 20px;
+  box-sizing: border-box;
+  max-width: 800px;
+  width: 100%;
+  box-shadow: 0px 0px 10px 0px rgb(254, 252, 252);
+}
+p {
+  text-align: center;
+  color: rgb(233, 202, 202);
+  font-size: 26px;
+}
+.list {
+  padding: 30px;
+  box-shadow: 0px 0px 5px 0px rgb(249, 248, 248);
+  &-item {
+    margin-bottom: 20px;
+    padding: 10px;
+    font-size: 20px;
+    font-style: italic;
+    color: #100404;
+    background: #b079b1;
+    box-shadow: 0px 0px 5px 0px rgb(249, 248, 248);
+  }
+  &-item:hover {
+    background: #eacdea;
+  }
+}
+.add-item {
+  margin: auto;
+  text-align: center;
+  max-width: 400px;
+  width: 100%;
+}
+input {
+  width: 100%;
+  height: 35px;
+  padding: 10px;
+  margin: 30px 0;
+  border: none;
+  border-radius: 20px;
+  box-shadow: 0px 0px 5px 0px rgb(249, 248, 248);
+  outline: none;
+  font-size: 18px;
+}
+.add-btn {
+  margin: auto;
+  text-align: center;
+}
+button {
+  padding: 15px;
+  color: #0d033e;
+  font-weight: 700;
+  cursor: pointer;
+  background: #eacdea;
+  border: 2px solid rgb(36, 4, 35);
+  font-size: 16px;
+  border-radius: 15px 50px;
+}
+button:hover {
+  background: rgb(14, 3, 49);
+  color: #d97fd9;
+  border: none;
+  transition: 0.2s;
 }
 </style>
